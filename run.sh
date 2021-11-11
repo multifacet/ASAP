c6269ff4cffb6ac7eca1a56aca3fc221e0eb7006
SujayYadalam94 <sujay.yadalam@gmail.com>
2021-11-10 23:20:08 -0600

    misc: Script changes and new files added
    Change-Id: I0f0cd2660cc8ac255284634bc7d36321e89a6a37

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export M5_PATH=/mydata
+export M5_PATH=$(pwd)
 
 if [ $# -eq 0 ]
 then
@@ -27,4 +27,4 @@ else
     cores=5
 fi
 
-sudo build/X86/gem5.fast -d results/baseline/$1 configs/example/fs.py --pmem --pmem-pwq --persist-buffers --pmem-wr-latency=60ns --mem-size=48GB --cpu-type=X86KvmCPU --kernel=/mydata/vmlinux_12 --disk-image=$image --ruby --asap -n $cores --script=scripts/$1.rcS
+sudo build/X86/gem5.fast -d results/baseline/$1 configs/example/fs.py --pmem --pmem-pwq --persist-buffers --pmem-wr-latency=60ns --mem-size=48GB --cpu-type=X86KvmCPU --kernel=$(pwd)/vmlinux_12 --disk-image=$image --ruby --asap -n $cores --script=scripts/$1.rcS
